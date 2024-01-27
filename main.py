0cdb018a5c22300dfe39d7836c198c90b05e0965
Ugla <cattugla@yandex.ru>
2024-01-27 17:03:48 +0700

    Functionality update
    Added feature to edit coffee and add new items of coffee.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from sys import argv, exit
+import sys
 from io import StringIO
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget
 from PyQt5 import uic
 
 import sqlite3
@@ -13,6 +13,10 @@ class Espresso(QMainWindow):
         self.initUI()
         self.db_connector = sqlite3.connect(".\coffee.sqlite")
         self.cursor = self.db_connector.cursor()
+        self.row_data = []
+        self.editor = None
+        self.col_names = ["ID", "название сорта", "степень обжарки", "молотый/в зернах",
+                          "описание вкуса", "цена", "объем упаковки"]
 
         self.load_data()
         return None
@@ -20,27 +24,93 @@ class Espresso(QMainWindow):
     def initUI(self) -> None:
         with open(file="main.ui", mode='r', encoding="utf-8") as template:
             uic.loadUi(StringIO(template.read()), self)
+        self.table.cellDoubleClicked.connect(self.gather_cell_data_and_edit)
+        self.addRec_menuItem.triggered.connect(self.add_new_rec)
         return None
 
     def load_data(self) -> None:
-        col_names = ["ID", "название сорта", "степень обжарки", "молотый/в зернах",
-                     "описание вкуса", "цена", "объем упаковки"]
+        
         rows = self.cursor.execute("select * from coffee").fetchall()
         self.table.clearContents()
-        self.table.setHorizontalHeaderLabels(col_names)
+        self.table.setHorizontalHeaderLabels(self.col_names)
         self.table.setRowCount(len(rows))
         rows = [tuple(map(str, [elem for elem in row])) for row in rows]
-        print(rows)
         for i in range(len(rows)):
             for j in range(len(rows[i])):
                 self.table.setItem(i, j, QTableWidgetItem(rows[i][j]))
-        
                 
         return None
+
+    def gather_cell_data_and_edit(self, row_index) -> None:
+        row_data = [self.table.item(row_index, j).text() for j in range(self.table.columnCount())]
+        self.editor = AddOrEditCoffee(self, row_data)
+        self.editor.show()
+        return None
+
+    def add_new_rec(self) -> None:
+        print("hey!")
+        self.editor = AddOrEditCoffee(self, [])
+        self.editor.show()
+        print("ho!")
+        return None
+
+
+class AddOrEditCoffee(QWidget):
+    def __init__(self, parent, coffee_data) -> None:
+        super().__init__()
+        self.parent = parent
+        self.initUI()
+        self.db_connector = sqlite3.connect(".\coffee.sqlite")
+        self.cursor = self.db_connector.cursor()
+        self.edit_id = -1
+        if coffee_data is not None and len(coffee_data) > 0:
+            self.edit_id = coffee_data[0]
+            self.name_txtEd.setText(coffee_data[1])
+            self.burn_txtEd.setText(coffee_data[2])
+            self.blend_txtEd.setText(coffee_data[3])
+            self.taste_txtEd.setText(coffee_data[4])
+            self.price_txtEd.setText(coffee_data[5])
+            self.vol_txtEd.setText(coffee_data[6])
+        else:
+            pass
+        return None
     
+    def initUI(self) -> None:
+        with open(file="addEditCoffeeForm.ui", mode='r', encoding="utf-8") as template:
+            uic.loadUi(StringIO(template.read()), self)
+        self.addEdit_btn.clicked.connect(self.add_or_edit_record)
+        return None
+
+    def add_or_edit_record(self) -> None:
+        data = [self.name_txtEd.text(), self.burn_txtEd.text(), self.blend_txtEd.text(),
+                self.taste_txtEd.text(), self.price_txtEd.text(), self.vol_txtEd.text()]
+        if self.edit_id != -1:  # update existing record
+            sql_cmd = "UPDATE coffee SET " +\
+                      "name=" + '\'' + data[0] + '\'' + ',' +\
+                      "burn_degree=" + '\'' + data[1] + '\'' + ',' +\
+                      "blend=" + '\'' + data[2] + '\'' + ',' +\
+                      "taste_descr=" + '\'' + data[3] + '\'' + ',' +\
+                      "price=" + data[4] + ',' +\
+                      "vol=" + '\'' + data[5] + '\'' +\
+                      " WHERE id=" + str(self.edit_id) + ';'
+            
+            self.cursor.execute(sql_cmd)
+        else:  # add new record
+            sql_cmd = "INSERT INTO coffee (name, burn_degree, blend, taste_descr, price, vol) VALUES(?, ?, ?, ?, ?, ?)"
+            self.cursor.execute(sql_cmd, data)
+        self.db_connector.commit()
+        self.parent.load_data()
+        self.close()
+        return None
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
 
 if __name__ == "__main__":
-    app = QApplication(argv)
+    sys.excepthook = except_hook
+    app = QApplication(sys.argv)
     ex = Espresso()
     ex.show()
-    exit(app.exec())
+    sys.exit(app.exec())
